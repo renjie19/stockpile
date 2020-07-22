f0aa71603809e25d8e6d6533b6753572de5fe0d7
Lourence <johnlourenceg@gmail.com>
2020-07-22 18:42:13 +0800

    initial project commit

---
+++ src/main/java/com/renjie/io/stockpile/StockPileApplication.java
@@ -0,0 +1,13 @@
+package com.renjie.io.stockpile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockPileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockPileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renjie/io/stockpile/StockPileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renjie.io.stockpile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockPileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
